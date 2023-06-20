e55c4d44ddb9b3fcd98f335715d5674f08d17bff
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-06-20 13:43:38 +0000

    code added

---
+++ PW-PPT-Program-Assignment-Solutions/01.DSA/02.Lecture-2_Arrays/02_question_1_solution.cpp
@@ -0,0 +1,41 @@
+/*
+Question 1
+Given an integer array nums of 2n integers, group these integers into n pairs (a1, b1), (a2, b2),..., (an, bn) such that the sum of min(ai, bi) for all i is maximized. Return the maximized sum.
+
+Example 1:
+Input: nums = [1,4,3,2]
+Output: 4
+
+Explanation: All possible pairings (ignoring the ordering of elements) are:
+
+1. (1, 4), (2, 3) -> min(1, 4) + min(2, 3) = 1 + 2 = 3
+2. (1, 3), (2, 4) -> min(1, 3) + min(2, 4) = 1 + 2 = 3
+3. (1, 2), (3, 4) -> min(1, 2) + min(3, 4) = 1 + 3 = 4
+So the maximum possible sum is 4
+*/
+
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int arrayPairSum(vector<int>& nums) {
+    sort(nums.begin(), nums.end());
+    int sum = 0;
+    for (int i = 0; i < nums.size(); i += 2) {
+        sum += nums[i];
+    }
+    return sum;
+}
+
+int main() {
+    vector<int> nums = {1, 4, 3, 2};
+
+    int result = arrayPairSum(nums);
+
+    cout << result << endl;
+
+    return 0;
+}
+
