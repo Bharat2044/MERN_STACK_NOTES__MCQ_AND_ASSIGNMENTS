29c2f22c3bfaa3474135686c7c1e5edf77d40a04
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-06-20 13:36:12 +0000

    code added

---
+++ PW-PPT-Program-Assignment-Solutions/01.DSA/01.Lecture-1_Arrays/06_question_5_solution.cpp
@@ -0,0 +1,60 @@
+/*
+Q5. You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+
+Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+
+The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+
+Example 1:
+Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+Output: [1,2,2,3,5,6]
+
+Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
+The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
+*/
+
+
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+    int i = m - 1; 
+    int j = n - 1; 
+    int k = m + n - 1; 
+    
+    while (i >= 0 && j >= 0) {
+        if (nums1[i] > nums2[j]) {
+            nums1[k] = nums1[i];
+            i--;
+        } 
+        else {
+            nums1[k] = nums2[j];
+            j--;
+        }
+        k--;
+    }
+    
+    while (j >= 0) {
+        nums1[k] = nums2[j];
+        j--;
+        k--;
+    }
+}
+
+int main() {
+    vector<int> nums1 = {1, 2, 3, 0, 0, 0};
+    int m = 3;
+    vector<int> nums2 = {2, 5, 6};
+    int n = 3;
+
+    merge(nums1, m, nums2, n);
+
+    // Print the merged array nums1
+    for (int i = 0; i < m + n; i++) 
+        cout << nums1[i] << " ";
+    cout << endl;
+
+    return 0;
+}
