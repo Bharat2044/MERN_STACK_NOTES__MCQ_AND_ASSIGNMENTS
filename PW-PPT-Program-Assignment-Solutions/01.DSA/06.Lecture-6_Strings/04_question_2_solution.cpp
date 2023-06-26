124d023ec7a05d4e0663fe77ba67cb2a3f6516f0
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-06-26 12:59:45 +0000

    code added

---
+++ PW-PPT-Program-Assignment-Solutions/01.DSA/06.Lecture-6_Strings/04_question_2_solution.cpp
@@ -0,0 +1,61 @@
+/*
+Question 2
+
+You are given an m x n integer matrix matrix with the following two properties:
+
+- Each row is sorted in non-decreasing order.
+- The first integer of each row is greater than the last integer of the previous row.
+
+Given an integer target, return true *if* target *is in* matrix *or* false *otherwise*.
+
+You must write a solution in O(log(m * n)) time complexity.
+
+Example 1:
+Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
+
+Output: true
+*/
+
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+bool searchMatrix(vector<vector<int>>& matrix, int target) {
+    int m = matrix.size();
+    int n = matrix[0].size();
+
+    int left = 0;
+    int right = m * n - 1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        int midValue = matrix[mid / n][mid % n];
+
+        if (midValue == target) {
+            return true;
+        } else if (midValue < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    return false;
+}
+
+int main() {
+    vector<vector<int>> matrix = {
+        {1, 3, 5, 7},
+        {10, 11, 16, 20},
+        {23, 30, 34, 60}
+    };
+    int target = 3;
+
+    bool found = searchMatrix(matrix, target);
+
+    cout << (found ? "true" : "false") << endl;
+
+    return 0;
+}
