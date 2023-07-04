593abb78614492199151ea4ed03ec2b2e0eee529
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-07-04 14:01:11 +0000

    code added

---
+++ PW-PPT-Program-Assignment-Solutions/01.DSA/08.Lecture-8_Recursion/07_question_5_solution.cpp
@@ -0,0 +1,68 @@
+/*
+Question 5
+
+Given an array of characters chars, compress it using the following algorithm:
+
+Begin with an empty string s. For each group of consecutive repeating characters in chars:
+
+- If the group's length is 1, append the character to s.
+- Otherwise, append the character followed by the group's length.
+
+The compressed string s should not be returned separately, but instead, be stored in the input character array chars. Note that group lengths that are 10 or longer will be split into multiple characters in chars.
+
+After you are done modifying the input array, return *the new length of the array*.
+
+You must write an algorithm that uses only constant extra space.
+
+Example 1:
+Input: chars = ["a","a","b","b","c","c","c"]
+
+Output: Return 6, and the first 6 characters of the input array should be: ["a","2","b","2","c","3"]
+
+Explanation:
+The groups are "aa", "bb", and "ccc". This compresses to "a2b2c3".
+*/
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int compress(vector<char>& chars) {
+    int n = chars.size();
+    int writeIdx = 0;
+    int count = 1;
+
+    for (int i = 1; i < n; i++) {
+        if (chars[i] == chars[i - 1]) {
+            count++;
+        } else {
+            chars[writeIdx++] = chars[i - 1];
+            if (count > 1) {
+                string countStr = to_string(count);
+                for (char c : countStr) {
+                    chars[writeIdx++] = c;
+                }
+            }
+            count = 1;
+        }
+    }
+
+    chars[writeIdx++] = chars[n - 1];
+    if (count > 1) {
+        string countStr = to_string(count);
+        for (char c : countStr) {
+            chars[writeIdx++] = c;
+        }
+    }
+
+    return writeIdx;
+}
+
+int main() {
+    vector<char> chars = {'a', 'a', 'b', 'b', 'c', 'c', 'c'};
+    int newLength = compress(chars);
+
+    cout << newLength << endl;
+
+    return 0;
+}
