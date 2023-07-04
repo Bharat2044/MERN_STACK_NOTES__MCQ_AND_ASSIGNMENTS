28f2287d76d87e85ecc5be980f0f93e4af435e73
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-07-04 14:01:07 +0000

    code added

---
+++ PW-PPT-Program-Assignment-Solutions/01.DSA/08.Lecture-8_Recursion/06_question_4_solution.cpp
@@ -0,0 +1,16 @@
+/*
+ Question 4
+
+You need to construct a binary tree from a string consisting of parenthesis and integers.
+
+The whole input represents a binary tree. It contains an integer followed by zero, one or two pairs of parenthesis. The integer represents the root's value and a pair of parenthesis contains a child binary tree with the same structure.
+You always start to construct the left child node of the parent first if it exists.
+
+Input: s = "4(2(3)(1))(6(5))"
+
+Output: [4,2,6,3,1,5]
+*/
+
+
+
+
